36f8fe28a616b7e44dcf102dcb2f35b0a812fe2e
Zachary Wagner <wagner2z@udml16.cs.uregina.ca>
2019-02-05 11:16:09 -0600

    Added rotations

--- New Unity Project/Assets/NewBehaviourScript.cs
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -13,32 +13,53 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.DownArrow))
+        move();
+    }
+
+    void move()
+    {
+        
+        Cursor.lockState = CursorLockMode.Locked;
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             Vector3 position = this.transform.position;
             position.x--;
             this.transform.position = position;
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             Vector3 position = this.transform.position;
             position.x++;
             this.transform.position = position;
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             Vector3 position = this.transform.position;
             position.z++;
             this.transform.position = position;
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             Vector3 position = this.transform.position;
             position.z--;
             this.transform.position = position;
         }
+
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+
+        if (this.transform.rotation.eulerAngles.z != 0)
+        {
+            transform.Rotate(0, 0, -this.transform.eulerAngles.z);
+        }
+
+        if (this.transform.rotation.eulerAngles.x < 50 && this.transform.rotation.eulerAngles.x > -50)
+        {
+            transform.Rotate(-v, h, 0);
+        }
     }
 }
+
